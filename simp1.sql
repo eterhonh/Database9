d0d54e09c5c2f1762f8ca92a17ff42c2fb8736a3
Yuyh21 <61218535+Yuyh21@users.noreply.github.com>
2020-12-05 22:46:10 -0500

    Update simp1.sql

--- simp1.sql
+++ simp1.sql
@@ -1,5 +1,4 @@
-SELECT Campus_Eats_Fall2020.order.order_id, total_price, ratings_id, driver_id, driver_comment 
+SELECT Campus_Eats_Fall2020.order.order_id, total_price, ratings_id, driver_id, driver_comment
 FROM Campus_Eats_Fall2020.order, Campus_Eats_Fall2020.ratings
-WHERE total_price > 5 AND ratings_id > 3
-ORDER BY total_price desc
-LIMIT 5;
+WHERE driver_rating = '5'
+ORDER BY total_price desc;
