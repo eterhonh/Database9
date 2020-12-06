0c4285588c9535f582691531452fb2efc0cbcc56
Yuyh21 <61218535+Yuyh21@users.noreply.github.com>
2020-12-05 22:39:33 -0500

    Add files via upload

---
+++ adva1.sql
@@ -0,0 +1,8 @@
+SELECT Campus_Eats_Fall2020.order.order_id, total_price, ratings_id, driver_id, driver_comment 
+FROM Campus_Eats_Fall2020.order, Campus_Eats_Fall2020.ratings
+WHERE total_price > 5 AND ratings_id > 3
+ORDER BY total_price desc
+LIMIT 5;
+
+
+
